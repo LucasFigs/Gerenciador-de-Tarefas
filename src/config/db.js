9c8de2e5af0190c6d55783c3105a88c86ffcf71d
Lucas Figueredo <lucasfig.oliv@gmail.com>
2025-10-01 10:40:11 -0300

    teste

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,17 @@
-// db.js
-import pkg from 'pg';
-const { Pool } = pkg;
+import mysql from "mysql2/promise";
+import dotenv from 'dotenv';
 
-export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
-});
+dotenv.config();
 
 export async function getConnection() {
-  return await pool.connect();
+  return await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    ssl: {
+      rejectUnauthorized: true
+    }
+  });
 }
